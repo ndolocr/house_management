71ebf66a725ed04605c8dffa8c376f015d4cad26
ndolocr <crndolo@gmail.com>
2022-08-13 07:43:45 +0300

    In this commit, the house app is created and the model file of the app populated. The dashboard file created as well as adding the house app in the installed files in settings file. Static files were also created.

--- house_management/house/models.py
+++ house_management/house/models.py
@@ -60,14 +60,14 @@ class Room(models.Model):
 class Tenant(models.Model):
     id_number = models.CharField( max_length = 255 )
     last_name = models.CharField( max_length = 255 )
-    first_name = models.CharField( max_length = 255 )    
+    first_name = models.CharField( max_length = 255 )
+    room = models.ForeignKey( Room, on_delete=models.CASCADE )
     id_card_back_image = models.ImageField( upload_to='id_card' )
     id_card_front_image = models.ImageField( upload_to='id_card' )
     middle_name = models.CharField( max_length = 255, null = True )
     passport_size_photo = models.ImageField( upload_to='passport' )
     phone_number = models.CharField( max_length = 255, null = True )
     email_address = models.CharField( max_length = 255, null = True )
-    
     
     updated_on = models.DateTimeField( auto_now = True)
     created_on = models.DateTimeField( auto_now_add = True)
