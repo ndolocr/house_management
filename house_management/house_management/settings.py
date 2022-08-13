4d81145dd21a5f2d69fe129809662ffa37cadcb9
ndolocr <crndolo@gmail.com>
2022-08-13 07:45:48 +0300

    Setting static files

--- house_management/house_management/settings.py
+++ house_management/house_management/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]  
-STATIC_ROOT = STATIC_ROOT = BASE_DIR / "staticfiles"  # new
+STATIC_ROOT = BASE_DIR / "staticfiles"  # new
 
 
 # Default primary key field type
